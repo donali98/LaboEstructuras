1fdbf238fe87d10534dafff294027baad586a215
Donali <00084417@uca.edu.sv>
2018-09-28 08:11:38 -0600

    Eje 4

--- ejercicio_4.js
+++ ejercicio_4.js
@@ -1,12 +1,8 @@
-function sumarExtremos(arr = [1,2,3,4,5,6,7,8]){
-    if(arr.length%2 == 0){
-        for(let i = 0; i<(arr.length / 2) ; i++){
-            console.log(arr[i] + arr[(arr.length-1) - i]);
-        }
-
-    }else{
-
+function sumarExtremos(arr = [1,2,2]){
+    for(let i = 0; i<(arr.length / 2) ; i++){
+        console.log(arr[i] + arr[(arr.length-1) - i]);
     }
+
 }
 
 sumarExtremos();
